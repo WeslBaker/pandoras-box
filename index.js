cde60abb6f453601c486734b646b899d94a88991
Aapple21 <aapplegate12@icloud.com>
2023-10-27 07:16:01 -0500

    quotes

--- index.js
+++ index.js
@@ -1,52 +1,17 @@
 //Dad Jokes
-const jokeDiv = document.getElementById("joke");
-const jokeButton = document.getElementById("jokeBtn");
+// const jokeDiv = document.getElementById("joke");
+// const jokeButton = document.getElementById("jokeBtn");
 
-jokeButton.addEventListener("click", loadJoke );
+// jokeButton.addEventListener("click", loadJoke );
 
-loadJoke();
+// loadJoke();
 
-sampledata = {
-    "joke":"What did the mama bullet say to the daddy bullet? We're having a BB.",
-    "likes" : 45
-};
+// sampledata = {
+//     "joke":"What did the mama bullet say to the daddy bullet? We're having a BB.",
+//     "likes" : 45
+// };
     
-async function loadJoke() {
-
-    const config = {
-        headers : {
-            Accept: "application/json"
-        }
-    };
-
-    const result = await fetch("https://icanhazdadjoke.com", config);
-    const data = await result.json();
-
-    jokeDiv.innerHTML = data.joke;
-
-}
-
-
-
-// const QuoteDiv = document.getElementById("quote");
-// const QuoteButton = document.getElementById("quoteBtn");
-
-// QuoteButton.addEventListener("click", getapi );
-
-// const api_url ="https://zenquotes.io/api/quotes/";
-
-// async function getapi(url)
-// {
-//   const response = await fetch(url);
-//   var data = await response.json();
-//   console.log(data);
-// }
-
-// getapi(api_url);
-
-// loadquote();
-
-// async function loadquote() {
+// async function loadJoke() {
 
 //     const config = {
 //         headers : {
@@ -54,10 +19,39 @@ async function loadJoke() {
 //         }
 //     };
 
-//     const resultquote = await fetch("https://zenquotes.io/api/quotes/", config);
-//     const dataquote = await resultquote.json();
+//     const result = await fetch("https://icanhazdadjoke.com", config);
+//     const data = await result.json();
 
-//     QuoteDiv.innerHTML = dataquote.quote;
+//     jokeDiv.innerHTML = data.joke;
 
 // }
 
+
+
+const QuoteDiv = document.getElementById("quote");
+const QuoteButton = document.getElementById("quoteBtn");
+
+QuoteButton.addEventListener("click", loadquote );
+
+// const api_url ="https://api-ninjas.com/api/quotes";
+
+
+loadquote();
+
+async function loadquote() {
+
+    const config = {
+        headers : {
+            Accept: "application/json"
+        }
+    };
+
+    const resultquote = await fetch("https://www.chunchunye.com/random-quote/", config);
+    const dataquote = await resultquote.json();
+
+    QuoteDiv.innerHTML = dataquote.quote;
+
+}
+
+
+{/* <div class="miniwebtoolwidget" data-calculator="random-quote-generator" data-width="380"><div class="miniwebtoolh">Random Quote Generator</div><p><a href="https://miniwebtool.com/random-quote-generator/" target="_blank">Random Quote Generator ©miniwebtool.com</a></p></div><script async src="https://miniwebtool.com/embed.js?v=1"></script> */}
